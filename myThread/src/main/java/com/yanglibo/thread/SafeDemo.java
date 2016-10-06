8e081d4d116b6bb84c70e4b5decc3363c1afaf40
yanglibo <yanglibo@qgutech.com>
2016-10-06 17:11:18 +0800

    多线程共享数据问题

--- myThread/src/main/java/com/yanglibo/thread/SafeDemo.java
+++ myThread/src/main/java/com/yanglibo/thread/SafeDemo.java
@@ -3,13 +3,13 @@ package com.yanglibo.thread;
 /**
  * Created by Administrator on 2016/10/6.
  */
-public class UnSafeDemo {
+public class SafeDemo {
     private int[] arr = {10, 10, 10, 10, 10};
 
     final Bank bank = new Bank();
 
     public static void main(String[] args) {
-        UnSafeDemo unSafeDemo = new UnSafeDemo();
+        SafeDemo unSafeDemo = new SafeDemo();
         for (int j = 0; j < 10000; j++) {
             new Thread(unSafeDemo.new TransferRunner()).start();
         }
@@ -26,8 +26,8 @@ public class UnSafeDemo {
 
     class Bank {
 
-        //此处未添加 synchronized关键字，输出的结果不会一直为：total=50
-        void transfer(int fromAccount, int toAccount, int amount) {
+        //此处添加 synchronized关键字，输出的结果会一直为：total=50
+        synchronized void transfer(int fromAccount, int toAccount, int amount) {
             arr[fromAccount] -= 10;
             arr[toAccount] += 10;
             System.out.println("total=" + getTotal());
